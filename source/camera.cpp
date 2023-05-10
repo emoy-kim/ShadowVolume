20d8b1a57bd5d451ca2f459b37f3b3a7eef291b7
jeesun <emoy.kim@gmail.com>
2023-05-10 11:56:23 +0900

    feat: change the camera view

--- source/camera.cpp
+++ source/camera.cpp
@@ -2,8 +2,8 @@
 
 CameraGL::CameraGL() :
    CameraGL(
-      glm::vec3(100.0f, 200.0f, 200.0f),
-      glm::vec3(0.0f, 100.0f, 0.0f),
+      glm::vec3(500.0f, 750.0f, 400.0f),
+      glm::vec3(0.0f, 500.0f, 0.0f),
       glm::vec3(0.0f, 1.0f, 0.0f)
    )
 {
